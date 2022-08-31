3782f68304ce533a92d12a45693c3189d0dda03c
The Floating String <laurencel2001@gmail.com>
2022-08-30 22:42:37 -0400

    Add datafiles and implement markers with test data

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const router = express.Router();
 
 const portNum = 3000;
 
+app.use(express.static(__dirname+'/node_modules/leaflet/dist'));
+app.use(express.static(__dirname+'/public'));
+
+const dataFile = require(__dirname+"/public/static/data/test_data.json");
+console.log(dataFile)
+
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname+'/views/index.html'));
 })
 
-app.use(express.static(__dirname+'/node_modules/leaflet/dist'));
-app.use(express.static(__dirname+'/public'));
+
+app.get("/getData", function (req, res) {
+	console.log("sending datafile...")
+	res.json(dataFile);
+})
+
 
 app.listen(portNum);
 console.log('Running at port '+portNum.toString());
--- public/src/custom/main.js
+++ public/src/custom/main.js
@@ -1,4 +1,5 @@
-console.log("hello world");
+const Http = new XMLHttpRequest();
+const url = "/getData"
 
 var map = L.map('map').setView([45.5, -73.5], 10);
 
@@ -6,3 +7,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+Http.open("GET", url);
+Http.send()
+
+Http.onreadystatechange=(e)=>{
+    try {
+        let jsonVar = JSON.parse(Http.responseText);
+        for (let i=0; i<jsonVar.data.length; i++) {
+            console.log(jsonVar.data[i].coordinates.x);
+            console.log(jsonVar.data[i].coordinates.y);
+            console.log('');
+
+            L.marker([
+                jsonVar.data[i].coordinates.x,
+                jsonVar.data[i].coordinates.y
+            ]).addTo(map);
+        }
+        // console.log(jsonVar.data[0]);
+    } catch (e) {
+        console.log(e);
+    }
+    
+}
