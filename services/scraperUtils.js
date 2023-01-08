402ec77fe28fa15bceb17c9ee03c89f39932c573
The Floating String <laurencel2001@gmail.com>
2023-01-08 12:36:33 -0500

    Add Selenium scraping WIP

--- services/scraperUtils.js
+++ services/scraperUtils.js
@@ -3,6 +3,13 @@ const { Builder, By } = require('selenium-webdriver');
 const seleniumWebdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
+// // Config
+
+// let chromeCapabilities = seleniumWebdriver.Capabilities.chrome();
+// let chromeOptions = { 'args': ['--disable-infobars'] };
+// chromeCapabilities.set('chromeOptions', chromeOptions);
+// chreom
+
 async function scrapeDonneesMontreal() {
 
     let driver = new Builder().forBrowser('chrome')
@@ -11,28 +18,62 @@ async function scrapeDonneesMontreal() {
     await driver.get("https://donnees.montreal.ca/ville-de-montreal/etablissements-alimentaires");
 
     let title = await driver.getTitle();
-    console.log(title);
-
-    // let h1 = await driver.findElement(By.css('h1'));
-    // console.log(await h1.getText());
-
-    // let tables = await driver.findElements(By.css('table'));
-    // console.log(tables);
+    // console.log(title);
 
     let table = await driver.findElement(By.className('info block pb-12'))
     let tBody = await table.findElement(By.css('tbody'));
 
     let tRows = await tBody.findElements(By.css('tr'));
-
     let tRowCsv = tRows[1];
-    // let tCell = tRowCsv.findElements(By.css('td'));
 
+    let tCells = await tRowCsv.findElements(By.css('td'));
+    let tCell4 = tCells[4];
 
-    console.log(await tRowCsv.getText());
+    let aElem = await tCell4.findElement(By.css("a[onclick^='sendGAEvent']"))
+
+    console.log(aElem);
+    console.log(await aElem.getCssValue())
+    await aElem.click();
+
+    // let aItem = await tCell4.findElement(By.css("a[onclick^='javascript: sendGAEvent']")).click();
+
+    // console.log(aItem.findElement(By.css('')))
+
+    // const csvUrl = await aItem.getAttribute('href');
+
+    // console.log(csvUrl);
+    // aItem.click();
+
+    // let driver2 = new Builder().forBrowser('chrome')
+    // .setChromeOptions(new chrome.Options().addArguments('--headless'))
+    // .build();
+
     
-    // console.log(await tCell.getText());
 
-    // console.log(table);
+    // console.log(csvUrl);
+    // await driver2.get(csvUrl).then( res => {
+    //     console.log(res);
+    // }).catch( e => {
+    //     console.log('error');
+    //     console.log(e);
+    // });
+
+    // const csvFile = await driver2.getPageSource();
+    // console.log(csvFile);
+
+
+
+    // await driver.get(csvUrl);
+    // const csvFile = await driver.getPageSource();
+
+    // console.log(csvFile);
+
+    // csvFile = driver.g();
+
+    // console.log(await csvFile.)
+            
+    // chrome.get(csvUrl);
+    // csvFile = chrome
 
     await driver.quit();
 
@@ -46,5 +87,8 @@ function scrape211() {
 }
 
 function scrapeAnagraph() {
+
+    let driver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
+
     return null;
 }
