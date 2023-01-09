ce9f355d5f8921bc5b3bb43ae6d8020bc9be4f0f
The Floating String <laurencel2001@gmail.com>
2023-01-08 21:13:47 -0500

    Clean up commented code

--- services/createDb.js
+++ services/createDb.js
@@ -7,16 +7,6 @@ const config = require('../config')
 
 const pool = new pg.Pool(config.db);
 
-// Uncomment the following section to delete the table organizations
-
-/*
-const q_drop = `
-DROP TABLE organizations;
-`
-pool.query(q_drop);
-// */
-
-
 const clearDatabase = () => {
 
     const poolDb = new pg.Pool(config.db);
@@ -56,64 +46,8 @@ const printDatabase = () => {
     poolDb.end();
 }
 
-const q_init = `
-CREATE TABLE IF NOT EXISTS organizations(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(1000),
-    address     VARCHAR(1000),
-    city        VARCHAR(1000),
-    state       VARCHAR(1000),
-    country     VARCHAR(1000),
-    category    VARCHAR(1000),
-    openStatus  VARCHAR(1000),
-    latitude    NUMERIC(20, 18),
-    longitude   NUMERIC(20, 18),
-    dataSource  VARCHAR(1000),
-    createdAT   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO organizations(
-    name,
-    address,
-    city,
-    state,
-    country,
-    category,
-    openStatus,
-    latitude, 
-    longitude,
-    dataSource
-)
-VALUES (
-    'Restaurant Name',
-    '1 Main Street',
-    'Montreal',
-    'Quebec',
-    'Canada',
-    'food',
-    'open',
-    45,
-    -73,
-    'Données Montréal'
-);
-`
-
-// pool.query(q_init).then((res, err) => {
-    // console.log(err, res);
-// });
-
-// const q_select = `
-// SELECT * FROM organizations
-// `;
-
-// pool.query(q_select).then((res, err) => {
-//     console.log(res.rows);
-// })
-
-// console.log(getQuery(q_select));
 
 pool.end();
 
-// clearDatabase();
 
 module.exports = { clearDatabase, printDatabase }
--- services/scraperUtils.js
+++ services/scraperUtils.js
@@ -3,14 +3,12 @@ const { Builder, By } = require('selenium-webdriver');
 const seleniumWebdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-// // Config
+// Config
 
-// let chromeCapabilities = seleniumWebdriver.Capabilities.chrome();
-// let chromeOptions = { 'args': ['--disable-infobars'] };
-// chromeCapabilities.set('chromeOptions', chromeOptions);
-// chreom
 
 async function scrapeDonneesMontreal() {
+
+    /** TODO: work in progress */
 
     let driver = new Builder().forBrowser('chrome')
     .setChromeOptions(new chrome.Options().addArguments('--headless'))
@@ -34,47 +32,6 @@ async function scrapeDonneesMontreal() {
     console.log(aElem);
     console.log(await aElem.getCssValue())
     await aElem.click();
-
-    // let aItem = await tCell4.findElement(By.css("a[onclick^='javascript: sendGAEvent']")).click();
-
-    // console.log(aItem.findElement(By.css('')))
-
-    // const csvUrl = await aItem.getAttribute('href');
-
-    // console.log(csvUrl);
-    // aItem.click();
-
-    // let driver2 = new Builder().forBrowser('chrome')
-    // .setChromeOptions(new chrome.Options().addArguments('--headless'))
-    // .build();
-
-    
-
-    // console.log(csvUrl);
-    // await driver2.get(csvUrl).then( res => {
-    //     console.log(res);
-    // }).catch( e => {
-    //     console.log('error');
-    //     console.log(e);
-    // });
-
-    // const csvFile = await driver2.getPageSource();
-    // console.log(csvFile);
-
-
-
-    // await driver.get(csvUrl);
-    // const csvFile = await driver.getPageSource();
-
-    // console.log(csvFile);
-
-    // csvFile = driver.g();
-
-    // console.log(await csvFile.)
-            
-    // chrome.get(csvUrl);
-    // csvFile = chrome
-
     await driver.quit();
 
     return null;
